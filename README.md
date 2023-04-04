9826bf0913d323b178786a08452443c7a9689c9f
minkh0512 <minkh0512@naver.com>
2023-04-04 18:49:18 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# astroBasic
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-nhcvkp)
---
+++ src/components/Banner.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function Banner() {
+  return (
+    <div className="section__main section__main-banner">
+      <div className="section__main-inner">
+        <a
+          href="http://rpp.gmarket.co.kr/?exhib=95298"
+          className="link__banner"
+          data-montelena-acode="200003547"
+        >
+          <img
+            src="//image.gmarket.co.kr/hanbando/202303/133152b2-80a0-48fe-98bc-14eb46120707.png"
+            alt="마트 빅페스타 매일 찾아오는 15% 쿠폰 무제한 발급"
+            className="image"
+          />
+          <noscript>
+            <img
+              src="//image.gmarket.co.kr/hanbando/202303/133152b2-80a0-48fe-98bc-14eb46120707.png"
+              alt="마트 빅페스타 매일 찾아오는 15% 쿠폰 무제한 발급"
+              className="image"
+            />
+          </noscript>
+        </a>
+      </div>
+    </div>
+  );
+}
